fa62eb16249e9fe90b145d609ac5b295289d894d
Daniel Le Pelley <daniellepelley@gmail.com>
2015-01-15 21:32:02 +0000

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name daniellepelleygithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the daniellepelleygithubioApp
+ */
+angular.module('daniellepelleygithubioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name daniellepelleygithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the daniellepelleygithubioApp
+ */
+angular.module('daniellepelleygithubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

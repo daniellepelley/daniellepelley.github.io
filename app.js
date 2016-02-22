2228f73b57d5b7959fd419c63594b1b3033aee89
daniellepelley@gmail.com <daniellepelley@gmail.com>
2016-02-22 22:57:35 +0000

    Tv Channels test

---
+++ app.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module("app", []);
+
+app.controller("channelsCtrl", function($scope) {
+    $scope.channels = [
+        {
+            url: "http://www.bbc.co.uk/iplayer/live/bbcone",
+            img: "bbc1.png"
+        },
+        {
+            url: "http://www.bbc.co.uk/iplayer/live/bbctwo",
+            img: "bbc2.png"
+        },
+        {
+            url: "http://www.bbc.co.uk/iplayer/live/bbcthree",
+            img: "bbc3.png"
+        }
+    ];
+});

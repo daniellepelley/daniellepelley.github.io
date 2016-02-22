6d0b8d34600fa54b28ae9a407c4ba2ebc6499a5d
daniellepelley@gmail.com <daniellepelley@gmail.com>
2016-02-22 23:50:35 +0000

    Added itv

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 ﻿var app = angular.module("app", []);
 
-app.controller("channelsCtrl", function($scope) {
-    $scope.channels = [
-        {
-            url: "http://www.bbc.co.uk/iplayer/live/bbcone",
-            img: "bbc1.png"
-        },
-        {
-            url: "http://www.bbc.co.uk/iplayer/live/bbctwo",
-            img: "bbc2.png"
-        },
-        {
-            url: "http://www.bbc.co.uk/iplayer/live/bbcnews",
-            img: "bbcnews.png"
-        }
-    ];
+app.controller("channelsCtrl", function ($scope) {
+    $scope.providers = [
+            {
+                title: "BBC",
+                channels: [
+               {
+                   url: "http://www.bbc.co.uk/iplayer/live/bbcone",
+                   img: "bbc1.png"
+               },
+               {
+                   url: "http://www.bbc.co.uk/iplayer/live/bbctwo",
+                   img: "bbc2.png"
+               },
+               {
+                   url: "http://www.bbc.co.uk/iplayer/live/bbcnews",
+                   img: "bbcnews.png"
+               }]
+            },
+            {
+                title: "ITV",
+                channels: [
+               {
+                   url: "http://www.itv.com/hub/itv",
+                   img: "itv.png"
+               },
+               {
+                   url: "http://www.itv.com/hub/itv2",
+                   img: "itv2.jpg"
+               }]
+        }];
 });

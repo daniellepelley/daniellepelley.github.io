de4e5c602676f5cc53d41621f01bd681e8b18308
daniellepelley@gmail.com <daniellepelley@gmail.com>
2016-02-24 06:32:49 +0000

    Added Channel 4

--- app.js
+++ app.js
@@ -29,5 +29,26 @@ app.controller("channelsCtrl", function ($scope) {
                    url: "http://www.itv.com/hub/itv2",
                    img: "itv2.jpg"
                }]
-        }];
+            },
+            {
+                title: "Channel 4",
+                channels: [
+                {
+                    url: "http://www.channel4.com/now/c4",
+                    img: "c4.png"
+                },
+                {
+                    url: "http://www.channel4.com/now/e4",
+                    img: "e4.png"
+                },
+                {
+                    url: "http://www.channel4.com/now/f4",
+                    img: "film4.png"
+                },
+                {
+                    url: "http://www.channel4.com/now/m4",
+                    img: "more4.png"
+                }]
+            }
+    ];
 });
